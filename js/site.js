0548fbd69388db7e55615bcff85dcea143b39da1
mflerin <marc@flerin.com>
2014-11-09 08:07:40 +0000

    structure finished

---
+++ js/site.js
@@ -0,0 +1,16 @@
+(function($) {
+	
+	$(document).ready(function() {
+      $('#pagepiling').pagepiling({
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3','page4'],
+        navigation: {
+          'textColor': '#f2f2f2',
+          'bulletsColor': '#ccc',
+          'position': 'right',
+          'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
+        }
+      });
+    });
+
+})(jQuery);
